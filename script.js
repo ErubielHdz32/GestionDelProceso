bcd21297175214dd710c3ab0370b9e4ee519f81c
ErubielHdz32 <ErubielHdz32@users.noreply.github.com>
2026-02-24 05:36:44 +0000

    deploy: 3d8da387a12ba4f25f4f47292354c34da9d8dfb7

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const titulo = document.getElementById("titulo");
+
+btn.addEventListener("click", () => {
+    titulo.textContent = "Texto cambiado con JavaScript 🚀";
+});
